447a9784a3f1ef46b64cae99f53648c166c746d7
jhon_cun <jhon_cun@cun.edu.co>
2022-10-23 14:08:45 -0500

    Version 1

---
+++ Tipos_Datos/src/tipos_datos/Tipos_Datos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos_datos;
+
+/**
+ *
+ * @author JENG-PC
+ */
+public class Tipos_Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       byte a = 1;
+       short b =12345;
+       int c = 56;
+       long d = 45;
+       float e = 4.1234567f;
+       double f = 4.12345678910111213d;
+       char g = 'a';
+       boolean h = true;
+       
+        System.err.println("byte: "+a+"\nShort: "+b+"\nint: "+c+"\nlong:"+d+"\nfloat: "+e+"\ndouble: "+f+"\nchar: "+g+"\nboolean: "+h);
+       
+           
+    }
+    
+}
